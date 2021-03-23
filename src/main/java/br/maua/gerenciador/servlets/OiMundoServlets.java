03236de561153ce4dcad3621d6f15747142ee95c
Mathvivas <mathvivas@hotmail.com>
2021-03-23 11:00:18 -0300

    Bem-vindo.html

--- src/main/java/br/maua/gerenciador/servlets/OiMundoServlets.java
+++ src/main/java/br/maua/gerenciador/servlets/OiMundoServlets.java
@@ -1,5 +1,29 @@
 package br.maua.gerenciador.servlets;
 
-public class OiMundoServlets {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Ctrl Espace para opções
+
+@WebServlet( urlPatterns = "/oi")		// Fornece um apelido no endereço URL qu remete ao Servlet do gerenciador
+public class OiMundoServlets extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		// Devolve um conteúdo HTML
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi, Mundo! Parabéns! Você escreveu o  primeiro Servlets!");
+		out.println("</body>");
+		out.println("</html>");
+	}
 }
