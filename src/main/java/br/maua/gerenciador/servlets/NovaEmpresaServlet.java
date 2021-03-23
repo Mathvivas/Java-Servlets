83fe5e27a63e55047a3cf295bdab7554123dba35
Mathvivas <mathvivas@hotmail.com>
2021-03-23 11:29:12 -0300

    Utilizando Request e Response

--- src/main/java/br/maua/gerenciador/servlets/NovaEmpresaServlet.java
+++ src/main/java/br/maua/gerenciador/servlets/NovaEmpresaServlet.java
@@ -1,6 +1,8 @@
 package br.maua.gerenciador.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,14 +13,21 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class NovaEmpresaServlet
  */
 @WebServlet("/novaEmpresa")
-public class NovaEmpresaServlet extends HttpServlet {
+public class NovaEmpresaServlet extends HttpServlet  {
+	
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		// http://localhost:8080/gerenciador/novaEmpresa?nome=Alura
+		
+		System.out.println("Cadastrando Nova Empresa");
+		
+		String nomeEmpresa = request.getParameter("nome");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><body>Empresa " + nomeEmpresa + " Cadastrada com Sucesso!</body></html>");
 	}
-
 }
