0b467fdda930d4c621bf331ec34a845476011488
Mathvivas <mathvivas@hotmail.com>
2021-07-07 15:28:07 -0300

    Rotas do Status

--- SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/repository/PedidoRepository.java
+++ SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/repository/PedidoRepository.java
@@ -1,10 +1,14 @@
 package br.maua.mvc.mudi.repository;
 
 import br.maua.mvc.mudi.model.Pedido;
+import br.maua.mvc.mudi.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository                              // Classe que está lidando e o id dessa classe
-public interface PedidoRepository extends JpaRepository<Pedido, Long> { }
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByStatus(StatusPedido status);
+}
