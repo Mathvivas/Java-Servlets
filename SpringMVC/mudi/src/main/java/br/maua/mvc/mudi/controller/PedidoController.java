9b8dfd33df35595c9dedff581fc724af7db74ac5
Mathvivas <mathvivas@hotmail.com>
2021-07-07 12:32:51 -0300

    Implementação do Bean Validation

--- SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/controller/PedidoController.java
+++ SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/controller/PedidoController.java
@@ -5,9 +5,12 @@ import br.maua.mvc.mudi.model.Pedido;
 import br.maua.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("pedido")
@@ -21,8 +24,12 @@ public class PedidoController {
         return "pedido/formulario";
     }
 
-    @PostMapping("novo")
-    public String novo(RequisicaoNovoPedido req) {
+    @PostMapping("novo")    // @Valid executa a validação
+    public String novo(@Valid RequisicaoNovoPedido req, BindingResult result) {
+
+        if ( result.hasErrors() ) {
+            return "pedido/formulario";
+        }
 
         Pedido pedido = req.toPedido();
         pedidoRepository.save(pedido);      // Salvo no banco de dados
--- SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -2,10 +2,17 @@ package br.maua.mvc.mudi.dto;
 
 import br.maua.mvc.mudi.model.Pedido;
 
+import javax.validation.constraints.NotBlank;
+
 public class RequisicaoNovoPedido {
 
+    @NotBlank
     private String nomeProduto;
+
+    @NotBlank
     private String urlProduto;
+
+    @NotBlank
     private String urlImagem;
     private String descricao;
 
