e2c274078d7dbd7f27941a7bb76afa5e9272f315
Mathvivas <mathvivas@hotmail.com>
2021-07-09 15:35:01 -0300

    Ordenação por data, paginação e otimização com cache

--- SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/controller/HomeController.java
+++ SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/controller/HomeController.java
@@ -4,11 +4,12 @@ import br.maua.mvc.mudi.model.Pedido;
 import br.maua.mvc.mudi.model.StatusPedido;
 import br.maua.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -24,18 +25,21 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model) {
-        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);
+        Sort sort = Sort.by("dataEntrega").descending();
+        PageRequest paginacao = PageRequest.of(0, 2, sort);
+
+        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
         model.addAttribute("pedidos", pedidos);
         return "home";
     }
 
-    @GetMapping("/{status}")
-    public String porStatus(@PathVariable("status") String status, Model model) {
-        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-        model.addAttribute("pedidos", pedidos);
-        model.addAttribute("status", status);
-        return "home";
-    }
+//    @GetMapping("/{status}")
+//    public String porStatus(@PathVariable("status") String status, Model model) {
+//        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+//        model.addAttribute("pedidos", pedidos);
+//        model.addAttribute("status", status);
+//        return "home";
+//    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public String onError() {
--- SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/repository/PedidoRepository.java
+++ SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package br.maua.mvc.mudi.repository;
 
 import br.maua.mvc.mudi.model.Pedido;
 import br.maua.mvc.mudi.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +14,8 @@ import java.util.List;
 @Repository                              // Classe que está lidando e o id dessa classe
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    List<Pedido> findByStatus(StatusPedido status);
+    @Cacheable("books")
+    List<Pedido> findByStatus(StatusPedido status, Pageable pageable);
 
     @Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
     List<Pedido> findAllByUsuario(@Param("username")String username);
