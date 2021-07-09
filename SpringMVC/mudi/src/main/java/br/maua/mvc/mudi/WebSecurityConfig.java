8f909370eeca6596b67ccdb9338098d6a0671005
Mathvivas <mathvivas@hotmail.com>
2021-07-09 12:39:30 -0300

    Escondendo a senha do usuário

--- SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/WebSecurityConfig.java
+++ SpringMVC/mudi/src/main/java/br/maua/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,26 @@
 package br.maua.mvc.mudi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -20,19 +28,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().formLogin(form -> form
                     .loginPage("/login")
+                    .defaultSuccessUrl("/home", true)
                     .permitAll())
                 .logout(logout -> logout.logoutUrl("/logout"));
     }
 
-    @Bean
     @Override
-    protected UserDetailsService userDetailsService() {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         UserDetails user =
-                User.withDefaultPasswordEncoder()
-                    .username("math")
-                    .password("123")
-                    .roles("ADM")
-                    .build();
-        return new InMemoryUserDetailsManager(user);
+                User.builder()
+                        .username("math")
+                        .password(encoder.encode("123"))
+                        .roles("ADM")
+                        .build();
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder)
+                .withUser(user);
     }
 }
