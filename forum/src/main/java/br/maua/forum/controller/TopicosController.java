d6720ac0cf54bf3458747aabb569ed9378a184fd
Mathvivas <mathvivas@hotmail.com>
2021-07-19 15:56:41 -0300

    Utilização de GET e POST

--- forum/src/main/java/br/maua/forum/controller/TopicosController.java
+++ forum/src/main/java/br/maua/forum/controller/TopicosController.java
@@ -1,21 +1,26 @@
 package br.maua.forum.controller;
 
 import br.maua.forum.controller.dto.TopicoDTO;
+import br.maua.forum.controller.form.TopicoForm;
 import br.maua.forum.modelo.Topico;
+import br.maua.forum.repository.CursoRepository;
 import br.maua.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController     // Por padrão, métodos possuem ResponseBody
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDTO> listar(String nomeCurso) {
         if ( nomeCurso == null ) {
             List<Topico> topicos = topicoRepository.findAll();
@@ -26,5 +31,12 @@ public class TopicosController {
             List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
             return TopicoDTO.converter(topicos);
         }
+    }
+
+    @PostMapping
+    // RequestBody pegará no corpo, não na URL
+    public void cadastrar(@RequestBody TopicoForm form) {
+        Topico topico = form.converter(cursoRepository);
+        topicoRepository.save(topico);
     }
 }
