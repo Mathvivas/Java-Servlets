e9edb01aeea28628bf22ab3b5fc7d7268ec7fc19
Mathvivas <mathvivas@hotmail.com>
2021-07-23 10:01:58 -0300

    Implementação de Paginação e getOneCountry

--- cities-api/src/main/java/br/maua/citiesapi/CountryResource.java
+++ cities-api/src/main/java/br/maua/citiesapi/CountryResource.java
@@ -3,11 +3,15 @@ package br.maua.citiesapi;
 import br.maua.citiesapi.countries.entities.Country;
 import br.maua.citiesapi.countries.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -17,7 +21,19 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        // http://localhost:8080/countries?page=0&size=5&sort=name,desc
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOneCountry(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if ( optional.isPresent() ) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
