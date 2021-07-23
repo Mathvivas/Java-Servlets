e4679c2988685014483f27c577abcf071b220526
Mathvivas <mathvivas@hotmail.com>
2021-07-23 09:26:32 -0300

    Criação do Projeto e Implementação do findAll

---
+++ cities-api/src/main/java/br/maua/citiesapi/CountryResource.java
@@ -0,0 +1,23 @@
+package br.maua.citiesapi;
+
+import br.maua.citiesapi.countries.entities.Country;
+import br.maua.citiesapi.countries.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryResource {
+
+    @Autowired
+    private CountryRepository repository;
+
+    @GetMapping
+    public List<Country> countries() {
+        return repository.findAll();
+    }
+}
