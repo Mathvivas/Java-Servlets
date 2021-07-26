52bcca8641c8cdec2319868d8d5cc38d043edf5f
Mathvivas <mathvivas@hotmail.com>
2021-07-26 12:41:58 -0300

    Configuração da Documentação com Swagger e SpringFox

--- Spring-Boot-Api/forum/src/main/java/br/maua/forum/config/security/SecurityConfiguration.java
+++ Spring-Boot-Api/forum/src/main/java/br/maua/forum/config/security/SecurityConfiguration.java
@@ -58,5 +58,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // Configurações de Recursos Estáticos (JS, CSS, imagens, etc)
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**",
+                        "/swagger-resources/**");
     }
 }
