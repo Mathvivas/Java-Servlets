93daa1a498b619e0119fb16d43d9e16e123174ab
Mathvivas <mathvivas@hotmail.com>
2021-07-27 10:12:50 -0300

    Realização de Testes com Spring Boot e MockMvc

---
+++ Spring-Boot-Api/forum/src/test/java/br/maua/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,36 @@
+package br.maua.forum.repository;
+
+import br.maua.forum.modelo.Curso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@ActiveProfiles("test")     // Carrega o application-test.properties
+public class CursoRepositoryTest {
+
+    @Autowired
+    private CursoRepository repository;
+
+    @Test
+    public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+        String nomeCurso = "HTML 5";
+        Curso curso = repository.findByNome(nomeCurso);
+        Assertions.assertTrue(true);
+        Assertions.assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+        String nomeCurso = "JPA";
+        Curso curso = repository.findByNome(nomeCurso);
+        Assertions.assertNull(curso);
+    }
+}
